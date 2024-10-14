cac4f98b5a14e6ccd7bb49328f3dbba7dca4ba57
tms-a-kawase <a-kawase@tms-e.co.jp>
2024-10-14 22:16:11 +0900

    kadai_006

---
+++ js/class-operation.js
@@ -0,0 +1,23 @@
+$(function () {
+    // $('#add').on('click', function () {
+    //     $('#target').addClass('sample');
+    // });
+    //
+    // $('#remove').on('click', function () {
+    //     $('#target').removeClass('sample');
+    // });
+    $('#toggle').on('click', function () {
+        $('#target').toggleClass('sample');
+    });
+
+    $('#has').on('click', function () {
+        let result = $('#target').hasClass('sample');
+
+        if (result) {
+            console.log('sampleクラスがついています');
+        }
+        else {
+            console.log('sampleクラスがついていません');
+        }
+    });
+});
---
+++ js/css-operation.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#red').on('click', function(){
+        $('#target').css('color','red');
+    });
+    $('#blue').on('click', function(){
+        $('#target').css('color','blue');
+    });
+});
