05d9f5965382f89057b91fd897e0d1b3ec14bfde
tms-a-kawase <a-kawase@tms-e.co.jp>
2024-10-17 21:30:20 +0900

    form

---
+++ js/form.js
@@ -0,0 +1,25 @@
+$('#check').on('click', function () {
+    console.log('名前：' + $('[name="username"]').val());
+    console.log('性別：' + $('[name="gender"]:checked').val());
+    console.log('血液型：' + $('[name="blood"]').val());
+});
+
+$('[name="hobby"]').on('change', function () {
+    if ($('[name="hobby"]:checked').length > 3) {
+        $(this).prop('checked', false);
+        alert('3つまでしか選択できません');
+    }
+});
+
+$(function () {
+
+    $('[name="username"]').on("input", function () {
+        let input = $(this).val();
+
+        if (input) {
+            $('#check').prop('disabled', false);
+        } else {
+            $('#check').prop('disabled', true);
+        }
+    })
+});
