94417d46d7af98daa53655272ea9e92356059382
tms-a-kawase <a-kawase@tms-e.co.jp>
2024-10-20 13:18:57 +0900

    aaa

---
+++ js/animation.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('#fadeOut').on('click', function () {
+        $('.box').fadeOut();
+        // $('.box').fadeOut(3000, function () {
+        //     alert('fadeOut完了');
+    });
+});
