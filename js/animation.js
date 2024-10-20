4f065f95209f98a905e487c37a03d579fbe6661b
tms-a-kawase <a-kawase@tms-e.co.jp>
2024-10-20 22:35:58 +0900

    kadai_010

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,20 @@
 $(function () {
     $('#fadeOut').on('click', function () {
-        $('.box').fadeOut();
-        // $('.box').fadeOut(3000, function () {
-        //     alert('fadeOut完了');
+        $('.box').css('opacity', 0);
     });
+    $('#fadeIn').on('click', function () {
+        $('.box').css('opacity', 1);
+    });
+    // $('#fadeToggle').on('click', function () {
+    //     $('.box').fadeToggle();
+    // });
+    $('#slideUp').on('click', function () {
+        $('.box').css('height', 0);
+    });
+    $('#slideDown').on('click', function () {
+        $('.box').css('height', 200);
+    });
+    // $('#slideToggle').on('click', function () {
+    //     $('.box').slideToggle();
 });
+
